3f7973b4b6b6dc7cff088f0b75bbf1b5dcaa8199
Rahul Sutradhar <rahulsd380@gmail.com>
2024-09-28 20:19:56 +0600

    project setup

---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+const DashboardLayout = ({children} : {children : ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default DashboardLayout;
